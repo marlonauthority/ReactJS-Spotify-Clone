1b42a667ad297fad15e4ec394aafea6b5066b4d9
marlondck <15050029+marlondck@users.noreply.github.com>
2019-06-05 18:09:44 -0400

    Styles da Sidebar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Routes />
+      <Sidebar />
     </>
   );
 }
