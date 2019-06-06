017fc1819c23a7935eea3e9f6b34ae031d747fb5
marlondck <15050029+marlondck@users.noreply.github.com>
2019-06-06 17:41:41 -0400

    Routes

---
+++ src/pages/browse/index.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import {
+  Container, Title, List, Playlist,
+} from './styles';
+
+export default function browse() {
+  return (
+    <Container>
+      <Title>Navegar</Title>
+
+      <List>
+        <Playlist href="">
+          <img
+            src="https://s9.limitedrun.com/images/1265295/v600_childish_gambino_awaken_my_love_lp.jpg"
+            alt="Playlist"
+          />
+          <strong>Wake for Love</strong>
+          <p>Sua playlist memoravel.</p>
+        </Playlist>
+
+        <Playlist href="">
+          <img
+            src="https://s9.limitedrun.com/images/1265295/v600_childish_gambino_awaken_my_love_lp.jpg"
+            alt="Playlist"
+          />
+          <strong>Wake for Love</strong>
+          <p>Sua playlist memoravel.</p>
+        </Playlist>
+
+        <Playlist href="">
+          <img
+            src="https://s9.limitedrun.com/images/1265295/v600_childish_gambino_awaken_my_love_lp.jpg"
+            alt="Playlist"
+          />
+          <strong>Wake for Love</strong>
+          <p>Sua playlist memoravel.</p>
+        </Playlist>
+
+        <Playlist href="">
+          <img
+            src="https://s9.limitedrun.com/images/1265295/v600_childish_gambino_awaken_my_love_lp.jpg"
+            alt="Playlist"
+          />
+          <strong>Wake for Love</strong>
+          <p>Sua playlist memoravel.</p>
+        </Playlist>
+      </List>
+    </Container>
+  );
+}
